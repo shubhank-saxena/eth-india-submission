4b083a51f2da758b4094073aeb18ae59d5add0e8
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2022-12-04 05:33:39 +0530

    Remove dune testing file

--- backend/dune_helpers/helpers.py
+++ backend/dune_helpers/helpers.py
@@ -1,10 +1,12 @@
 import requests
 from django.conf import settings
+import time
 
 API_KEY = settings.DUNE_API_KEY
 HEADER = {"x-dune-api-key" : API_KEY}
 
 BASE_URL = "https://api.dune.com/api/v1/"
+
 
 def make_api_url(module, action, ID):
     url = BASE_URL + module + "/" + ID + "/" + action
@@ -17,10 +19,22 @@ def execute_query(query_id):
 
     return execution_id
 
+def get_query_status(execution_id):
+    url = make_api_url("execution", "status", execution_id)
+    response = requests.get(url, headers=HEADER)
+
+    return response
+
 def get_query_results(query_id):
     execution_id = execute_query(query_id)
+    print(get_query_status(execution_id).json())
+    # print(execution_status)
 
     url = make_api_url("execution", "results", execution_id)
     response = requests.get(url, headers=HEADER)
 
-    return response
+    time.sleep(5)
+
+    return response['result']['rows']
+
+# get_query_results(str(1698919))
